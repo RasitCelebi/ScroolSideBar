58383c4180e74f825229e3567caa2efd466acd64
RasitCelebi <rasit_61@hotmail.com>
2023-03-03 15:44:53 +0300

    finished project

--- src/simple.js
+++ src/simple.js
@@ -1,8 +1,29 @@
 
-/* let xa = document.querySelector('.feat-btn');
+let clickEvent = document.querySelector('.myMenuUl');
 
-xa.addEventListener("click" ,function(){
-    console.log("aa")
-    xa.classList.toggleClass("display: block");} ); */
+clickEvent.addEventListener("click", function (event) {
+    console.log("number of Children : " + event.target.childNodes.length + " child");
 
-  
+    if (event.target.childNodes.length > 1) {
+        event.target.querySelector('ul').classList.toggle("hidden");
+        event.target.querySelector('ul').classList.toggle("block");
+    }
+});
+
+
+let clickEventSub = document.querySelectorAll('.mySubMenu');
+
+clickEventSub.forEach(item =>
+    item.addEventListener(
+        'click', function (event) {
+            event.target.parentNode.classList.toggle("hidden");
+            event.target.parentNode.classList.toggle("block");
+        }
+    ));
+
+
+
+
+
+
+
